b44c9d9fc9d777d032385206e5955d72a42688ee
allan-drew <allanjfmg@hotmail.com>
2022-09-18 20:21:12 -0300

    nested For loop

---
+++ 06 - Loops/06.01.04 - nested For loops (laços aninhados)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    for ( int i = 0; i < 2; ++i )
+    {
+        
+        for ( int j = 0; j < 3; ++j)
+        {
+            std::cout << "C++ knows how to repeat!\n";
+        }
+    
+    std::cout << std::endl;
+
+    }
+
+
+
+    return 0;
+}
