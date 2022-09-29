955dd35df0ed26527d040e25a1d80a29164a1356
allan-drew <allanjfmg@hotmail.com>
2022-09-29 09:31:43 -0300

    functions

---
+++ 14 - Functions/14.1 - Intro to functions/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+int main() {
+
+    ////////////////////////////////// FUNCTION ///////////////////////
+
+    // FUNCTION : A reusable piece of code that can take a number of optional inputs and produce some desirable output 
+
+/*
+    How a function looks in C++ : 
+
+        return_type function_name (parameter1, parameter2,...) {
+            Processing
+            return return_type; 
+        }
+
+    void ==> means that the functon is not going to return anything. Basically it means "nothing" or "no type"
+
+*/
+
+
+
+
+    return 0;
+}
