2be2256b8a249c7d9ac3e7749cbe0acf04d6a92e
allan-drew <allanjfmg@hotmail.com>
2022-10-04 10:52:50 -0300

    intro to functions

--- 14 - Functions/14.1 - Intro to functions/main.cpp
+++ 14 - Functions/14.1 - Intro to functions/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 
+void enterIntoClub(size_t age) // What you pass to the function when you are defining it is a parameter
+{   
+    if (age >= 18) 
+    {
+            std::cout << "You're " << age << " years old. Please proceed." << std::endl; 
+    } else {
+            std::cout << "You're " << age << ". Sorry, you're too young for this. No offense! " << std::endl;
+    }
+    
+    return ;
+}
+
 int main() {
 
     ////////////////////////////////// FUNCTION ///////////////////////
@@ -19,7 +31,8 @@ int main() {
 
 */
 
-
+    enterIntoClub ( 25 );
+    enterIntoClub ( 15 );
 
 
     return 0;
